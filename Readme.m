70f6504bebef02569217ce55ff09accc49113d55
asadul <asadul33489686@gmail.com>
2025-08-29 22:32:56 +0600

    add this

---
+++ Readme.m
@@ -0,0 +1,15 @@
+// getElementById vs getElementsByClassName
+
+1/  getElementById Selects one element using its ID one the other hand getElementsByClassName Selects all elements with the given class name.
+2/getElementById should be unique but getElementsByClassName are always same .
+
+
+// querySelector  vs querySelectorAll
+
+1/ querySelector returns a single element but querySelectorAll Returns a NodeList.
+
+2/querySelector selects the first element that matches a CSS selector. but querySelectorAll Selects all elements that match a CSS selector.
+
+// What is the difference between preventDefault() and stopPropagation() methods?
+
+1/preventDefault() stops the default action of an element from happening. but stopPropagation() stops the event from bubbling up (or capturing down) the DOM.  
